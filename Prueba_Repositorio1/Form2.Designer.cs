12b150fb96c77885b0f9991e47a762ab9dda69e8
07CHMK1PA111A20 <07CHMK1PA111A20@07CHMK1PA111A20>
2026-04-29 09:27:25 -0500

    Mejoras minimas en posición de botones.

--- Prueba_Repositorio1/Form2.Designer.cs
+++ Prueba_Repositorio1/Form2.Designer.cs
@@ -54,18 +54,18 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(587, 415);
+            this.button1.Location = new System.Drawing.Point(558, 404);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(104, 34);
             this.button1.TabIndex = 2;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(704, 415);
+            this.button2.Location = new System.Drawing.Point(686, 404);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(93, 34);
             this.button2.TabIndex = 3;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
